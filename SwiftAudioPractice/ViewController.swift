3bcdb03c4879e2d157aa2e2a2711fe43f934967a
LuLuSaBee <lulusabeetw@gmail.com>
2021-09-16 17:48:11 +0800

    [feature]: Make SliderBar with seeking

--- SwiftAudioPractice/ViewController.swift
+++ SwiftAudioPractice/ViewController.swift
@@ -9,19 +9,76 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
-    @IBOutlet var nameLabel: UILabel!
-    @IBOutlet var playButton: UIButton!
-
     var songData: SongData!
 
     let player = AVPlayer()
     var playerItem: AVPlayerItem!
     var songIndex = 0
     var isPlaying = true
+    private var playerItemContext = 0
+    var secondsOfSong: Double!
+    var timer: Timer!
+    var timeInterval = 0.1
+
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var playButton: UIButton!
+    @IBOutlet var sliderBar: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         playMusic()
+    }
+
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey: Any]?, context: UnsafeMutableRawPointer?) {
+        guard context == &playerItemContext else {
+            super.observeValue(forKeyPath: keyPath,
+                of: object,
+                change: change,
+                context: context)
+            return
+        }
+
+        if keyPath == #keyPath(AVPlayerItem.status) {
+            let status: AVPlayerItem.Status
+            if let statusNumber = change?[.newKey] as? NSNumber {
+                status = AVPlayerItem.Status(rawValue: statusNumber.intValue)!
+            } else {
+                status = .unknown
+            }
+
+            switch status {
+            case .readyToPlay:
+                sliderBar.maximumValue = Float(playerItem.duration.seconds)
+                setTimer()
+            default:
+                return
+            }
+        }
+    }
+
+    func setTimer() {
+        timer = Timer.scheduledTimer(timeInterval: timeInterval, target: self, selector: #selector(updateSliderBar), userInfo: nil, repeats: true)
+    }
+
+    func stopTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+
+    @objc func updateSliderBar() {
+        sliderBar.setValue(Float(playerItem.currentTime().seconds), animated: false)
+    }
+
+    @IBAction func slide(_ slider: UISlider) {
+        stopTimer()
+        sliderBar.setValue(slider.value, animated: false)
+        playerItem.seek(to: CMTime(seconds: Double(slider.value), preferredTimescale: 1000), completionHandler: seekFinished)
+    }
+
+    func seekFinished(isFinished: Bool) {
+        if isFinished {
+            setTimer()
+        }
     }
 
     @IBAction func playButtonClick(_ sender: Any) {
@@ -35,6 +92,7 @@ class ViewController: UIViewController {
         } else {
             songIndex -= 1
         }
+        stopTimer()
         playMusic()
     }
 
@@ -44,6 +102,7 @@ class ViewController: UIViewController {
         } else {
             songIndex += 1
         }
+        stopTimer()
         playMusic()
     }
 
@@ -51,6 +110,7 @@ class ViewController: UIViewController {
         let currentSong = songData.songSources[songIndex]
         guard let url = URL(string: currentSong) else { return }
         playerItem = AVPlayerItem(url: url)
+        playerItem.addObserver(self, forKeyPath: #keyPath(AVPlayerItem.status), options: [.new], context: &playerItemContext)
         player.replaceCurrentItem(with: playerItem)
 
         nameLabel.text = url.lastPathComponent
