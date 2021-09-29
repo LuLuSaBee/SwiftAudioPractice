23e8768706de9f57ef574def8af4df0f43a7c5c1
LuLuSaBee <lulusabeetw@gmail.com>
2021-09-29 17:42:31 +0800

    [style]: Change style by Roy, NG teaching

--- SwiftAudioPractice/ViewController.swift
+++ SwiftAudioPractice/ViewController.swift
@@ -15,7 +15,7 @@ import RxAVFoundation
 
 class ViewController: UIViewController {
     var songData: SongData!
-
+    
     private let player = AVPlayer()
     private var playerItem: AVPlayerItem!
     private var songIndex = 0
@@ -25,24 +25,30 @@ class ViewController: UIViewController {
     private var nowPlayingInfo = [String: Any]()
     private var sliderMovedValue: Float? = nil
     private let disposeBag = DisposeBag()
-
+    
     @IBOutlet var nameLabel: UILabel!
     @IBOutlet var previousButton: UIButton!
     @IBOutlet var playButton: UIButton!
     @IBOutlet var nextButton: UIButton!
     @IBOutlet var sliderBar: UISlider!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         prepareMusic()
         setupRemoteControll()
         setupNotifications()
-
+        
         subscribePlayerButtons()
         subscribePlayer()
         subscribeSliderBar()
+        
+        do {
+            try audioSession.setActive(true)
+        } catch {
+            print("error")
+        }
     }
-
+    
     private func setupRemoteControll() {
         UIApplication.shared.beginReceivingRemoteControlEvents()
         MPRemoteCommandCenter.shared().playCommand.addTarget { event in
@@ -62,162 +68,168 @@ class ViewController: UIViewController {
             return .success
         }
     }
-
+    
     private func perviousMusic() {
         songIndex = (songIndex - 1 + songData.songSources.count) % songData.songSources.count
         prepareMusic()
     }
-
+    
     private func nextMusic() {
         songIndex = (songIndex + 1) % songData.songSources.count
         prepareMusic()
     }
-
+    
     private func prepareMusic() {
         let currentSong = songData.songSources[songIndex]
         guard let url = URL(string: currentSong) else { return }
         playerItem = AVPlayerItem(url: url)
         subscribePlayItem()
         player.replaceCurrentItem(with: playerItem)
-
+        
         nameLabel.text = url.lastPathComponent
-
+        
         nowPlayingInfo[MPMediaItemPropertyTitle] = url.lastPathComponent
         nowPlayingInfo[MPMediaItemPropertyAlbumTitle] = "InternetAlbum"
         nowPlayingInfo[MPMediaItemPropertyArtist] = "Internet"
-        setNowPlayingInfo()
+        setNowPlayingInfoAndCurrentTime()
     }
-
-    func setNowPlayingInfo() {
-        nowPlayingInfo[MPMediaItemPropertyPlaybackDuration] = playerItem.duration.seconds
+    
+    private func setNowPlayingInfoAndCurrentTime() {
         nowPlayingInfo[MPNowPlayingInfoPropertyElapsedPlaybackTime] = playerItem.currentTime().seconds
         nowPlayingInfoCenter.nowPlayingInfo = nowPlayingInfo
     }
-
+    
+    private func setSliderMovedValue(value: Float?) {
+        sliderMovedValue = value
+    }
+    
     // MARK: - Rx
-    func subscribePlayItem() {
-        playerItem.rx.status.subscribe(onNext: { [setupPlaying] status in
+    private func subscribePlayItem() {
+        let setupPlaying: () -> Void = { [player, sliderBar] in
+            player.play()
+            sliderBar?.setValue(0, animated: false)
+        }
+        
+        playerItem.rx.status
+            .subscribe(onNext: { [setupPlaying] status in
+                switch status {
+                case .readyToPlay:
+                    setupPlaying()
+                default:
+                    return
+                }
+            })
+            .disposed(by: disposeBag)
+        
+        playerItem.rx.didPlayToEnd.subscribe(onNext: { [nextMusic] _ in nextMusic() }).disposed(by: disposeBag)
+        
+        playerItem.rx.duration
+            .map { $0.seconds }
+            .subscribe(onNext: { [weak self, setNowPlayingInfoAndCurrentTime] seconds in
+                self?.sliderBar.maximumValue = Float(seconds)
+                
+                self?.nowPlayingInfo[MPMediaItemPropertyPlaybackDuration] = seconds
+                setNowPlayingInfoAndCurrentTime()
+            }).disposed(by: disposeBag)
+    }
+    
+    private func subscribePlayer() {
+        let setupWhenPlayerTimeControlStatusChange: (AVPlayer.TimeControlStatus) -> Void = { [weak self, setNowPlayingInfoAndCurrentTime] status in
             switch status {
-            case .readyToPlay:
-                setupPlaying()
+            case .playing:
+                self?.playButton.setImage(UIImage(systemName: "pause.fill"), for: .normal)
+                self?.nowPlayingInfo[MPNowPlayingInfoPropertyPlaybackRate] = 1
+            case .paused:
+                self?.playButton.setImage(UIImage(systemName: "play.fill"), for: .normal)
+                self?.nowPlayingInfo[MPNowPlayingInfoPropertyPlaybackRate] = 0
             default:
                 return
             }
-        }).disposed(by: disposeBag)
-
-        playerItem.rx.didPlayToEnd.subscribe(onNext: { [nextMusic] _ in nextMusic() }).disposed(by: disposeBag)
-    }
-
-    func setupPlaying() {
-        player.play()
-        sliderBar.setValue(0, animated: false)
-        sliderBar.maximumValue = Float(playerItem.duration.seconds)
-
-        setNowPlayingInfo()
-
-        do {
-            try audioSession.setActive(true)
-        } catch {
-            print("error")
+            setNowPlayingInfoAndCurrentTime()
         }
-    }
-
-    func subscribePlayer() {
-        player.rx.timeControlStatus.subscribe(onNext: { [setupWhenPlayerTimeControlStatusChange] status in
-            setupWhenPlayerTimeControlStatusChange(status)
-        }).disposed(by: disposeBag)
-
+        
+        player.rx.timeControlStatus
+            .subscribe(onNext: { status in setupWhenPlayerTimeControlStatusChange(status) })
+            .disposed(by: disposeBag)
+        
+        let compareSlideAndPlayerSeconds: (CMTime) -> Float =  { [sliderMovedValue, setSliderMovedValue] value in
+            let seconds = Float(CMTimeGetSeconds(value))
+            
+            if sliderMovedValue == seconds {
+                setSliderMovedValue(nil)
+                return seconds
+            }
+            return sliderMovedValue ?? seconds
+        }
+        
         player.rx.periodicTimeObserver(interval: CMTime(seconds: timeInterval, preferredTimescale: CMTimeScale(NSEC_PER_SEC)))
             .map { [compareSlideAndPlayerSeconds] value in return compareSlideAndPlayerSeconds(value) }
             .bind(to: sliderBar.rx.value)
             .disposed(by: disposeBag)
     }
-
-    func compareSlideAndPlayerSeconds(value: CMTime) -> Float {
-        let seconds = Float(CMTimeGetSeconds(value))
-
-        if sliderMovedValue == seconds {
-            setSliderMovedValue(value: nil)
-            return seconds
-        }
-        return sliderMovedValue ?? seconds
-    }
-
-    func setupWhenPlayerTimeControlStatusChange(status: AVPlayer.TimeControlStatus) {
-        switch status {
-        case .playing:
-            playButton.setImage(UIImage(systemName: "pause.fill"), for: .normal)
-            nowPlayingInfo[MPNowPlayingInfoPropertyPlaybackRate] = 1
-        case .paused:
-            playButton.setImage(UIImage(systemName: "play.fill"), for: .normal)
-            nowPlayingInfo[MPNowPlayingInfoPropertyPlaybackRate] = 0
-        default:
-            return
-        }
-
-        setNowPlayingInfo()
-    }
-
-    func subscribePlayerButtons() {
-        playButton.rx.tap.subscribe(onNext: { [player] _ in
-            switch player.timeControlStatus {
-            case .playing:
-                player.pause()
-            case .paused:
-                player.play()
-            default:
-                return
-            }
-        }).disposed(by: disposeBag)
-
+    
+    private func subscribePlayerButtons() {
+        playButton.rx.tap
+            .subscribe(onNext: { [player] _ in
+                switch player.timeControlStatus {
+                case .playing:
+                    player.pause()
+                case .paused:
+                    player.play()
+                default:
+                    return
+                }
+            })
+            .disposed(by: disposeBag)
+        
         previousButton.rx.tap.subscribe(onNext: { [perviousMusic] _ in perviousMusic() }).disposed(by: disposeBag)
         nextButton.rx.tap.subscribe(onNext: { [nextMusic] _ in nextMusic() }).disposed(by: disposeBag)
     }
-
-    func setupNotifications() {
+    
+    private func setupNotifications() {
+        let handleInterruption: (Notification) -> Void = {[player] notification in
+            guard let userInfo = notification.userInfo,
+                  let typeValue = userInfo[AVAudioSessionInterruptionTypeKey] as? UInt,
+                  let type = AVAudioSession.InterruptionType(rawValue: typeValue) else {
+                      return
+                  }
+            
+            switch type {
+            case .began:
+                player.pause()
+            case .ended:
+                guard let optionsValue = userInfo[AVAudioSessionInterruptionOptionKey] as? UInt else { return }
+                let options = AVAudioSession.InterruptionOptions(rawValue: optionsValue)
+                if options.contains(.shouldResume) {
+                    player.play()
+                } else {
+                    return
+                }
+            default:
+                return
+            }
+        }
+        
         NotificationCenter.default.rx
             .notification(AVAudioSession.interruptionNotification)
             .subscribe(onNext: { [handleInterruption] notification in
-            handleInterruption(notification)
-        })
+                handleInterruption(notification)
+            })
             .disposed(by: disposeBag)
     }
-
-    func handleInterruption(_ notification: Notification) {
-        guard let userInfo = notification.userInfo,
-            let typeValue = userInfo[AVAudioSessionInterruptionTypeKey] as? UInt,
-            let type = AVAudioSession.InterruptionType(rawValue: typeValue) else {
-            return
-        }
-
-        switch type {
-        case .began:
-            player.pause()
-        case .ended:
-            guard let optionsValue = userInfo[AVAudioSessionInterruptionOptionKey] as? UInt else { return }
-            let options = AVAudioSession.InterruptionOptions(rawValue: optionsValue)
-            if options.contains(.shouldResume) {
-                player.play()
-            } else {
-                return
-            }
-        default:
-            return
-        }
-    }
-
-    func subscribeSliderBar() {
-        sliderBar.rx.controlEvent(.valueChanged).subscribe(onNext: { [sliderBar, setSliderMovedValue] _ in
-            setSliderMovedValue(sliderBar?.value)
-        }).disposed(by: disposeBag)
-
-        sliderBar.rx.controlEvent([.touchUpInside, .touchUpOutside]).subscribe(onNext: { [sliderBar, player, setNowPlayingInfo] _ in
-            player.seek(to: CMTime(seconds: Double(sliderBar!.value), preferredTimescale: CMTimeScale(NSEC_PER_SEC)), completionHandler: { _ in setNowPlayingInfo() })
-        }).disposed(by: disposeBag)
-    }
-
-    func setSliderMovedValue(value: Float?) {
-        sliderMovedValue = value
+    
+    private func subscribeSliderBar() {
+        sliderBar.rx.controlEvent(.valueChanged)
+            .subscribe(onNext: { [sliderBar, setSliderMovedValue] _ in
+                setSliderMovedValue(sliderBar?.value)
+            })
+            .disposed(by: disposeBag)
+        
+        sliderBar.rx.controlEvent([.touchUpInside, .touchUpOutside])
+            .subscribe(onNext: { [sliderBar, player, setNowPlayingInfoAndCurrentTime] _ in
+                player.seek(to: CMTime(seconds: Double(sliderBar!.value), preferredTimescale: CMTimeScale(NSEC_PER_SEC)), completionHandler: { _ in setNowPlayingInfoAndCurrentTime() })
+            })
+            .disposed(by: disposeBag)
     }
 }
 
